dea0ff06e88023bf6d262d3e531b02dd62272892
bethanykjenkins <51201055+bethanykjenkins@users.noreply.github.com>
2019-11-14 21:06:51 -0500

    separate class tostore listing data

--- lib/clissified/listings.rb
+++ lib/clissified/listings.rb
@@ -8,8 +8,30 @@ class Clissified::Listings
     @@all << self
   end
   
+    output = {}
+
+  doc = Nokogiri::HTML(open("https://www.uniqlo.com/us/en/men/sale"))
+  apt_address = doc.css("div.product-tile-info")
+
+  apt_address.each do |unit|
+    
+    apt_rent = product.css("div.product-pricing span.product-standard-price").text
+    apt_details = product.css("div.product-pricing span.product-sales-price").text
+    
+    output[apt_address.to_sym] = {
+      :apt_rent => apt_rent,
+      :apt_details => apt_details
+    }
+  end
+
+  output
+
+end
+  
   def self.all
     @@all
   end
 
 end
+
+
