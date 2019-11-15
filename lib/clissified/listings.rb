e4ed4833d58862cc2910343e592d7b7a2f1ede11
bethanykjenkins <51201055+bethanykjenkins@users.noreply.github.com>
2019-11-15 16:24:33 -0500

    Update listings.rb

--- lib/clissified/listings.rb
+++ lib/clissified/listings.rb
@@ -1,36 +1,19 @@
 class Clissified::Listings
-  attr_accessor :address, :rooms, :rent #create reader & writer methods for these attributes
 
- @@all = []
+	@@all = [ ]
+	attr_accessor :address #create reader & writer methods for these attributes
+	attr_reader :rent, :details
 
-  def initialize (address)
-    @address = address
-    @@all << self
-  end
-  
-    output = {}
+	def initialize (address, rent, details)
+		@address  = address
+		@rent = rent
+		@details = details
+		@@all << self #initialize method adds each instance to the array to keep track of what we created
+	end
 
-  doc = Nokogiri::HTML(open("https://www.uniqlo.com/us/en/men/sale"))
-  apt_address = doc.css("div.product-tile-info")
-
-  apt_address.each do |unit|
-    
-    apt_rent = product.css("div.product-pricing span.product-standard-price").text
-    apt_details = product.css("div.product-pricing span.product-sales-price").text
-    
-    output[apt_address.to_sym] = {
-      :apt_rent => apt_rent,
-      :apt_details => apt_details
-    }
-  end
-
-  output
-
-end
-  
-  def self.all
-    @@all
-  end
+	def self.all
+		@@all
+	end
 
 end
 
