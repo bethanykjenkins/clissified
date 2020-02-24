b3662b0056d300e54a455c0c03af3b2a844c42ab
bethanykjenkins <51201055+bethanykjenkins@users.noreply.github.com>
2020-02-24 16:45:18 -0500

    Update listings3.rb

--- lib/clissified/listings3.rb
+++ lib/clissified/listings3.rb
@@ -7,16 +7,6 @@ class Clissified::Listings
   def initialize
     self.nyc.all << self
   end
-  
-  
-  def list_apt
-    self.scrape_apts.each do |post|
-      apt = Clissified::Listings.new
-      apt.address = post.css("address.list-card-addr").text
-      apt.rent = post.css("div.list-card-price").text
-      apt.rooms = post.css("li").text
-    end
-  end
 
   def self.nyc
     @@all
