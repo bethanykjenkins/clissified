bf98231c4b78cb8965de709719b24c6fee27100f
bethanykjenkins <51201055+bethanykjenkins@users.noreply.github.com>
2020-02-28 13:45:55 -0500

    updated scraper

--- lib/clissified/scraper2.rb
+++ lib/clissified/scraper2.rb
@@ -1,28 +1,40 @@
 class Scraper
 
-  attr_accessor :name, :information #create reader & writer methods for these attributes
+  attr_reader :url
+  
+  @@all = [ ]
 
-  def self.nyc 
+  #def self.nyc 
     #class method
-    self.scrape_pets
+   # self.scrape_pets
     #scrape apartment data from site and return results
-  end
+ # end
 
-  def self.scrape_pets #instance method make use of url saved in object
-    pets = [] #instance variable to trace and access scraping objects
+  #def self.scrape_pets #instance method make use of url saved in object
+   # pets = [] #instance variable to trace and access scraping objects
     #creating scraper instance being saved to pet_scraper variable which stores instance of a scraper
     #return instance of scraper that runs url saved on this instance
-    pets << self.scrape_puppies
+   # pets << self.scrape_puppies
     
-    pets
-  end
+  #  pets
+  #end
+  
+    #def self.scrape_puppies
+      # html = Nokogiri::HTML(open("https://www.adoptapet.com/adoption_rescue/89777-new-beginning-animal-rescue-bronx-new-york"))
+      #  pet = self.new
+      #  pet.name = html.css("div.content").text.strip
+
+      #   pet
+    #  end
 
    def self.scrape_puppies
-    html = Nokogiri::HTML(open("https://www.adoptapet.com/adoption_rescue/89777-new-beginning-animal-rescue-bronx-new-york"))
-    pet = self.new
-    pet.name = html.css("div.content").text.strip
-
-    pet
+    html = Nokogiri::HTML(open(url))
+    html.css("div.content").each do |pet|
+    pet_text = pet.text.strip
+      Dog.new(pet_text)
   end
-  
+
 end
+  
+  pet_scraper = Scraper.new('https://www.adoptapet.com/adoption_rescue/89777-new-beginning-animal-rescue-bronx-new-york')
+  pet_scraper.scrape_puppies
