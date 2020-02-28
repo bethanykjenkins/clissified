b50747d912b1cb34a83384405f9c7f99a4371d01
bethanykjenkins <51201055+bethanykjenkins@users.noreply.github.com>
2020-02-27 21:07:24 -0500

    change apts to pets

--- lib/clissified/listings2.rb
+++ lib/clissified/listings2.rb
@@ -1,48 +1,35 @@
 
 class Clissified::Listings
-  attr_accessor :address, :rooms, :rent #create reader & writer methods for these attributes
+  attr_accessor :name, :breed, :information #create reader & writer methods for these attributes
 
   def self.nyc 
     #class method
     
-    self.scrape_apts
+    self.scrape_pets
     
     #scrape apartment data from site and return results
     
   end
 
-  def self.scrape_apts #instance method make use of url saved in object
-    apts = [] #instance variable to trace and access scraping objects
+  def self.scrape_pets #instance method make use of url saved in object
+    pets = [] #instance variable to trace and access scraping objects
 
     #creating scraper instance being saved to apt_scraper variable which stores instance of a scraper
     #return instance of scraper that runs url saved on  this instance
-    apts << self.scrape_trulia
-    apts << self.scrape_zillow
+    pets << self.scrape_puppies
     
-    apts
+    pets
   end
 
-   def self.scrape_trulia
-    html = Nokogiri::HTML(open("https://www.trulia.com/for_rent/Manhattan,NY/0-1500_price/APARTMENT,APARTMENT_COMMUNITY,APARTMENT%7CCONDO%7CTOWNHOUSE,CONDO,COOP,LOFT,TIC_type/"))
+   def self.scrape_puppies
+    html = Nokogiri::HTML(open("https://www.adoptapet.com/adoption_rescue/89777-new-beginning-animal-rescue-bronx-new-york"))
 
-    apt = self.new
-    
-    apt.address = html.css("div.Text__TextBase-sc-1i9uasc-0-div Text__TextContainerBase-sc-1i9uasc-1").text.strip
-    apt.rooms = html.css("div.Text__TextBase-sc-1i9uasc-0-div Text__TextContainerBase-sc-1i9uasc-1").text.strip
-    apt.rent = html.css("div.Text__TextBase-sc-1i9uasc-0-div Text__TextContainerBase-sc-1i9uasc-1").text.strip
-   binding.pry
-    apt
-  end
-  
-   def self.scrape_zillow
-    html = Nokogiri::HTML(open("https://www.zillow.com/homes/Manhattan-New-York-NY_rb/"))
+    pet = self.new
+    pet.address = html.css("grid-search-results").text.strip
+    pet.rooms = html.css("ul.list-card-details").text.strip
+    pet.rent = html.css("div.list-card-price").text.strip
 
-    apt = self.new
-    apt.address = html.css("grid-search-results").text.strip
-    apt.rooms = html.css("ul.list-card-details").text.strip
-    apt.rent = html.css("div.list-card-price").text.strip
-
-    apt
+    pet
   end
 
 end
