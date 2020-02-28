7908079e381a61e75aef885baa783903e2ab16bb
bethanykjenkins <51201055+bethanykjenkins@users.noreply.github.com>
2020-02-27 23:21:06 -0500

    Update scraper2.rb

--- lib/clissified/scraper2.rb
+++ lib/clissified/scraper2.rb
@@ -1,17 +1,28 @@
 class Scraper
 
-  def scrape_apts
-    doc = Nokogiri::HTML(open("https://www.zillow.com/homes/Manhattan-New-York-NY_rb/"))
+  attr_accessor :name, :information #create reader & writer methods for these attributes
+
+  def self.nyc 
+    #class method
+    self.scrape_pets
+    #scrape apartment data from site and return results
   end
 
-  
-  def list_apt
-    self.scrape_apts.each do |post|
-      apt = Clissified::Listings.new
-      apt.address = post.css("address.list-card-addr").text
-      apt.rent = post.css("div.list-card-price").text
-      apt.rooms = post.css("li").text
-    end
+  def self.scrape_pets #instance method make use of url saved in object
+    pets = [] #instance variable to trace and access scraping objects
+    #creating scraper instance being saved to pet_scraper variable which stores instance of a scraper
+    #return instance of scraper that runs url saved on this instance
+    pets << self.scrape_puppies
+    
+    pets
+  end
+
+   def self.scrape_puppies
+    html = Nokogiri::HTML(open("https://www.adoptapet.com/adoption_rescue/89777-new-beginning-animal-rescue-bronx-new-york"))
+    pet = self.new
+    pet.name = html.css("div.content").text.strip
+
+    pet
   end
   
 end
