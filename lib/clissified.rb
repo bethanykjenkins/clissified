524894921e7c7d8fdfe4fbeb22440a469ea8dcb0
bethanykjenkins <51201055+bethanykjenkins@users.noreply.github.com>
2020-02-28 20:21:50 -0500

    Update clissified.rb

--- lib/clissified.rb
+++ lib/clissified.rb
@@ -5,7 +5,8 @@ require 'pry'
 #acting as environment 
 
 require_relative "./clissified/version"
-require_relative "./clissified/listings2.rb"
+require_relative "./clissified/pet_scraper"
+require_relative "./clissified/pet"
 #require_relative "./clissified/listings3.rb"
 #require_relative "./clissified/scraper2.rb"
 require_relative "./clissified/cli"
